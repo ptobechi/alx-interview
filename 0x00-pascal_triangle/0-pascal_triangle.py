3f0bf026cd79c8c0d722f03156903dca0fe424fc
ptobechi <ptobechii@gmail.com>
2024-05-31 13:15:51 -0500

    paschal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,13 +6,16 @@ paschal triangle
 
 
 def pascal_triangle(n):
+    """
+    paschal triangle function
+    """
     if n <= 0:
         return []
 
     triangle = []
 
     for i in range(n):
-        row = [1] * (i + 1)  # Create a row with all elements initialized to 1
+        row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
