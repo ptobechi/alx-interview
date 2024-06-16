c29d94b6d827f212baae7b283c31ee4c4b53d1aa
ptobechi <ptobechii@gmail.com>
2024-06-15 19:00:18 -0500

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,6 +3,8 @@ import sys
 import signal
 
 # Global variables to store the metrics
+
+
 total_size = 0
 status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
