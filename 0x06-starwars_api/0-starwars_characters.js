0ccb959694b4296e0183a35ff18c6733bf2b58d8
ghstnet <jamespeteer6363@gmail.com>
2024-07-19 22:11:09 +0100

    start wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,42 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 
 const movieId = process.argv[2];
 const apiUrl = 'https://swapi-api.alx-tools.com/';
 
-axios.get(apiUrl + 'films/' + movieId)
-  .then(response => {
-    const charactersUrls = response.data.characters;
-    return Promise.all(charactersUrls.map(url => axios.get(url)));
+const getCharacterNames = (filmUrl) => {
+  return new Promise((resolve, reject) => {
+    request(filmUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const filmData = JSON.parse(body);
+        const characterUrls = filmData.characters;
+        resolve(characterUrls);
+      }
+    });
+  });
+};
+
+const getCharacterName = (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      }
+    });
+  });
+};
+
+getCharacterNames(apiUrl + 'films/' + movieId)
+  .then(characterUrls => {
+    return Promise.all(characterUrls.map(url => getCharacterName(url)));
   })
-  .then(charactersResponses => {
-    const characterNames = charactersResponses.map(response => response.data.name);
+  .then(characterNames => {
     characterNames.forEach(name => console.log(name));
   })
   .catch(error => {
